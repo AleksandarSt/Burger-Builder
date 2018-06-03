be420cfea840e188843d37f525ea73c4aa6c2f7d
AleksandarSt <A_V_S_@abv.bg>
2018-06-04 01:24:49 +0300

    Added Builder Controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,20 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(ingredientKey => {
             return [...Array(props.ingredients[ingredientKey])]
                 .map((_, index) => {
                     return <BurgerIngredient key={ingredientKey + index} type={ingredientKey} />
                 })
-        });
+        })
+        .reduce((arr, el) =>{
+            return arr.concat(el)
+        },[]);
+
+        if (transformedIngredients.length ===0) {
+            transformedIngredients=<div>Add some ingredients</div>
+        }
 
     return (
         <div className={classes.Burger}>
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
 import Wraper from '../../hoc/Wraper';
-import Burger from '../../components/Burger/Burger'
+import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 state = {
     ingredients:{
-        salad:1,
-        bacon:1,
-        cheese:1
+        salad:0,
+        bacon:0,
+        cheese:0
     }
 }
 
@@ -16,7 +17,7 @@ state = {
         return (
             <Wraper>
                 <Burger ingredients={this.state.ingredients}/>
-                <div>Burger Controls</div>
+                <BuildControls />
             </Wraper>
         )
     }
