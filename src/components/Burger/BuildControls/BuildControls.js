4c8af168d83424bc695f249d97a2242d6d56222b
AleksandarSt <A_V_S_@abv.bg>
2018-06-05 00:39:42 +0300

    Add ingridients logic

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -10,11 +10,14 @@ const controls = [
     { label: 'Bacon', type: 'bacon' }
 ]
 
-const buildControls = () => (
+const buildControls = (props) => (
     <div className={classes.BuildControls}>
         {controls.map(
             ctrl => (
-                <BuildControl key={ctrl.label} label={ctrl.label} />
+                <BuildControl 
+                key={ctrl.label} 
+                label={ctrl.label} 
+                added={()=>props.ingredientAdded(ctrl.type)}/>
             ))}
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,20 +4,47 @@ import Wraper from '../../hoc/Wraper';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGRIDIENT_PRICES={
+    salad:0.5,
+    cheese:0.4,
+    meat:1.3,
+    bacon:0.7
+}
+
 class BurgerBuilder extends Component {
 state = {
     ingredients:{
         salad:0,
         bacon:0,
-        cheese:0
-    }
+        cheese:0,
+        meat:0
+    },
+    totalPrice:4
+}
+
+addIngredientHandler = (type)=>{
+    const oldCount=this.state.ingredients[type];
+    const updatedCount=oldCount+1;
+    const updatedIngredients={...this.state.ingredients};
+    updatedIngredients[type]=updatedCount
+    const priceAddition=INGRIDIENT_PRICES[type];
+    const oldPrice=this.state.ingredients[type];
+    const newPrice = oldPrice+priceAddition;
+
+    this.setState({ingredients:updatedIngredients,
+        totalPrice:newPrice});
+}
+
+removeIngredientHandler = (type) =>{
+
 }
 
     render() {
         return (
             <Wraper>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <BuildControls 
+                ingredientAdded={this.addIngredientHandler}/>
             </Wraper>
         )
     }
