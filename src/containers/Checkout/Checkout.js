a97cdbd8945ada64c706812df8f37c5bafcdbda7
AleksandarSt <A_V_S_@abv.bg>
2018-12-17 16:21:27 +0200

    Refactoring added order reducer

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
@@ -15,8 +15,11 @@ class Checkout extends Component {
     }
 
     render() {
-        return (
-            <div>
+
+        let summary = <Redirect to="/" />
+
+        if (this.props.ings) {
+            summary = (<div>
                 <CheckoutSummary
                     ingredients={this.props.ings}
                     checkoutCancelled={this.checkoutCancelledHandler}
@@ -24,13 +27,20 @@ class Checkout extends Component {
                 <Route path={this.props.match.path + '/contact-data'}
                     component={ContactData} />
             </div>
+            );
+        }
+
+        return (
+            <div>
+                {summary}
+            </div>
         );
     }
 }
 
-const mapStateToProps = state =>{
+const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients
     }
 }
 
