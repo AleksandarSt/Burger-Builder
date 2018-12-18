bf7aef3830b892e3d225122c7bd63730845ad399
AleksandarSt <A_V_S_@abv.bg>
2018-12-18 17:58:07 +0200

    Refactoring fetchig orders from server through redux

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -42,3 +42,46 @@ export const purchaseInit = () => {
         type: actionsTypes.PURCHASE_INIT
     };
 };
+
+ export const fetchOrdersSuccess = (orders) => {
+     return {
+         type: actionsTypes.FETCH_ORDERS_SUCCESS,
+         orders: orders
+     };
+ };
+
+ export const fetcOrdersFail = (error) => {
+     return {
+         type: actionsTypes.FETCH_ORDERS_FAIL,
+         error: error
+     };
+ };
+
+ export const fetcOrdersStart = (error) => {
+    return {
+        type: actionsTypes.FETCH_ORDERS_START
+    };
+};
+
+export const fetchOrders = () => {
+    return dispatch => {
+
+        dispatch(fetcOrdersStart())
+
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                
+                dispatch(fetchOrdersSuccess(fetchedOrders));
+            })
+            .catch(err => {
+                dispatch(fetcOrdersFail(err));
+            });
+    }
+}
